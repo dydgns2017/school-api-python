8cdf53134d53511d0e193915c1b07bd67cac47bf
JungYonghoon <dydgns2017@gmail.com>
2018-11-13 00:38:15 +0900

    0.0.1

--- menu.py
+++ menu.py
@@ -6,8 +6,7 @@ try:
     schulCode=sys.argv[2] ## 학교 고유코드
     schulCrseCode=sys.argv[3] ## 학교 분류코드 (고등학교, 중학교, 초등학교)
     schulKndScCode="0" + str(sys.argv[3]) ## 학교 분류코드
-    ay=sys.argv[4]
-    mm=sys.argv[5]
+    schYm=sys.argv[4]
 except:
     print("""!!ERROR.... 
             코드 인자 값이 맞지 않거나 값이 이상합니다.
@@ -15,28 +14,34 @@ except:
     sys.exit(1)
 
 ##neis web requests
-URL="https://" + officeCode + "/sts_sci_sf01_001.do"
+URL="https://" + officeCode + "/sts_sci_md00_001.do"
 ##params = {'schulCode': 'B100000593', 'schulCrseScCode': '4', 'schulKndScCode' : '04', 'ay' : str(year)} 
-params = {'schulCode': str(schulCode), 'schulCrseScCode': str(schulCrseCode), 'schulKndScCode' : str(schulKndScCode), 'ay' : str(ay), 'mm' : str(mm)}
+params = {'schulCode': str(schulCode), 'schulCrseScCode': str(schulCrseCode), 'schulKndScCode' : str(schulKndScCode), 'schYm' : str(schYm)}
 response = requests.get(URL, params=params).text
 data = response[response.find("<tbody>"):response.find("</tbody>")]
 
 ## re 정규표현식으로 불필요한 데이터를 자르거나 변환
 regex = re.compile(r'[\n\r\t]')
 data=regex.sub('',data)
-rex = re.compile(r'<div class="textL">(.*?)</div>', re.S|re.M)
+rex = re.compile(r'<div>(.*?)</div>', re.S|re.M)
 data=rex.findall(data)
 
 file_json={}
 for dat in data:
-    date=dat[dat.find("<em>"):dat.find("</em>")][4:]
-    alert=dat[dat.find("<strong>"):dat.find("</strong>")][8:]
-    if date == "":
-        continue
-    if alert == "":
-        alert="None"
-    file_json.update({date : alert})
-
+    date=re.findall(r"[0-3][0-9]",dat[0:2])
+    menu=dat[dat.find("[중식]<br />"):]
+    if not date:
+        date=dat[0:1]
+        if date == "" or date == " ":
+            continue
+    if type(date) == list:
+        date=date[0]
+    menu = menu.split("<br />")
+    menu.remove(menu[0])
+    if not menu:
+        menu="None"
+    file_json.update({date : menu})
+    
 ##Json 생성
-with open('haksa.json', 'w', encoding='utf-8') as outfile:
+with open('menu.json', 'w', encoding='utf-8') as outfile:
        json.dump(file_json, outfile, sort_keys = False, indent=4, ensure_ascii=0)
